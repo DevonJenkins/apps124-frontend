34b7054a9ddc1c8e132f0e365f1b434b285cef97
DVNJNKNS <devonmjenkins3@gmail.com>
2022-04-11 17:42:59 -0400

    fix profile index bug

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,11 +1,14 @@
 import * as tokenService from '../services/tokenService'
-
-const BASE_URL = `${process.env.REACT_APP_API_URL}/api/profiles`
+const BASE_URL = `${process.env.REACT_APP_API_URL}/api/profiles/`
 
 async function getAllProfiles() {
   const res = await fetch(BASE_URL, {
     method:"GET",
-    headers: { Authorization: `Bearer ${tokenService.getToken()}` },
+    headers: { 
+      'content-type': 'application/json',
+      'Authorization': `Bearer ${tokenService.getToken()}`
+    },
+    body: JSON.stringify()
   })
   return await res.json()
 }
