a67109464f3cc8f8f136265e23588ae0126f99ba
DVNJNKNS <devonmjenkins3@gmail.com>
2022-04-13 17:09:41 -0400

    change favicon

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,7 +5,7 @@ const Landing = ({ user }) => {
     <main className={styles.container}>
       <h1>Welcome, {user ? user.name : 'friend'}</h1>
       {user ?
-        <h1> YOU are not a user </h1>
+        <h1> YOU are a user </h1>
       :
         <h1> YOU are not a user </h1>
       }
