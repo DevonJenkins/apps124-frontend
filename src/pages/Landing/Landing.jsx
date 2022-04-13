239c914a5c38314ab37ecf7ae3b80a7ad2354679
DVNJNKNS <devonmjenkins3@gmail.com>
2022-04-13 16:41:48 -0400

    deploy react

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -3,7 +3,12 @@ import styles from './Landing.module.css'
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      <h1>Welcome, {user ? user.name : 'friend'}</h1>
+      {user ?
+        <h1> YOU are not a user </h1>
+      :
+        <h1> YOU are not a user </h1>
+      }
     </main>
   )
 }
