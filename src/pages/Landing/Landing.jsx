5361f3cb663ab08b32b4353f10b8c01a339bf514
DVNJNKNS <devonmjenkins3@gmail.com>
2022-04-14 17:04:27 -0400

    fix nav bar bug

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,6 +4,10 @@ const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
       <h1>Hello {user ? user.name : 'world'}</h1>
+      {user ? <o>welcome back to apps 124</o>
+        :    
+        <p> Welcome to apps124</p>
+      }
       <p>please launch the terminal to get started</p>
     </main>
   )
