a5f5e0f043b6f0ae2686d78239f7ae6e560a6286
DVNJNKNS <devonmjenkins3@gmail.com>
2022-04-15 09:37:46 -0400

    indentation

--- src/pages/AppDetails/AppDetails.jsx
+++ src/pages/AppDetails/AppDetails.jsx
@@ -4,15 +4,12 @@ import { useParams } from "react-router-dom";
 //Services
 import * as appService from '../../services/appService'
 
-
 //Components
 import AppActions from "../AppActions/AppDelete";
-
 
 const AppDetails = ({ user }) => {
   const {id} = useParams()
   const [app, setApp] = useState(null)
-
 
   useEffect(() => {
     const fetchOne = async (app) => {
