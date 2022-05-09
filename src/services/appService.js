fe8bebb699d8163c771b272739e3bfdfb7678075
DVNJNKNS <devonmjenkins3@gmail.com>
2022-05-09 09:39:33 -0400

    remove dead code

--- src/services/appService.js
+++ src/services/appService.js
@@ -48,31 +48,6 @@ export const deleteOneApp = async (id) => {
   }
 }
 
-export const update = async (app) => {
-  // try {
-  //   const res = await fetch(`${BASE_URL}${app.id}`, {
-  //     method: 'PUT',
-  //     headers: {
-  //       'content-type': 'application/json',
-  //       'Authorization': `Bearer ${tokenService.getToken()}`
-  //     },
-  //     body: JSON.stringify(app)
-  //   })
-  //   return await res.json()
-  // } catch (error) {
-  //   throw error
-  // }
-}
 
-export const deleteOne = async (id) => {
-  // try {
-  //   const res = await fetch(`${BASE_URL}${id}`, {
-  //     method: "DELETE",
-  //     headers: { 'Authorization': `Bearer ${tokenService.getToken()}` }
-  //   })
-  //   return await res.json()
-  // } catch (error) {
-  //   throw error
-  // }
-}
+
 
